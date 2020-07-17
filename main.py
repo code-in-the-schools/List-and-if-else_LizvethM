6114ee9e815035905afa01f89108b1ec650f005e
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-17 18:09:15 +0000

    Part1 and 2_LizvethM

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 ###Part1
 userInput = input(str("Type anything here :D "))
 print(userInput.lower())
+print()
+
+
+
+
+###Part2
+userInput2 = input(str("Type anything here :D "))
+
+
+for i in range(len(userInput2)):
+  list= userInput2[i]
+  if(list=="a" or list=="e"or list=="i"or list== "o" or list== "u"):
+    print(str(list)+" | Vowel")
+  else:
+    print(str(list)+" | Consonant")
